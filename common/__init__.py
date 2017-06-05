c3174a8155fb66a4251dbb3b4ec9a5a687880522
xjtang <xjtang@bu.edu>
2017-06-05 14:24:27 -0400

    include new functions
    included enlarge, get_files, show_progress, and manage_batch

--- common/__init__.py
+++ common/__init__.py
@@ -1,11 +1,17 @@
 """ Module for common libarary
 """
 from .logger import log
-from .utility import date_to_doy, doy_to_date
+from .utility import (date_to_doy, doy_to_date, get_files, show_progress,
+                        manage_batch)
+from .data_processing import enlarge
 
 
 __all__ = [
     'log',
     'date_to_doy',
-    'doy_to_date'
+    'doy_to_date',
+    'enlarge',
+    'get_files',
+    'show_progress',
+    'manage_batch'
 ]
