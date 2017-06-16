7c7e1fe12e8879942fe846ee5701a4061efb4254
xjtang <xjtang@bu.edu>
2017-06-16 11:33:44 -0400

    Include new function gen_tn

--- tools/__init__.py
+++ tools/__init__.py
@@ -2,8 +2,10 @@
 """
 from .ftp_download import get_ftp
 from .http_download import download
+from .gen_thumbnail import gen_tn
 
 __all__ = [
     'get_ftp',
-    'download'
+    'download',
+    'gen_tn'
 ]
