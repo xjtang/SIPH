bb48097fec58558e2a4021f8e904788d48778c55
xjtang <xjtang@bu.edu>
2017-06-25 11:41:35 -0400

    Fixed the equation for calculating nob per month
    increment on percent clear, not percent cloud

--- visualization/cloud.py
+++ visualization/cloud.py
@@ -39,7 +39,7 @@ def nob_per_month(_file, verbose=False):
     for img in img_list:
         if verbose:
             log.info('Extracting nob from {}'.format(img[2].split('/')[-1]))
-        nobpm[d2d(img[0])[1], 1] += pcloud(img[2], 6)
+        nobpm[d2d(img[0])[1], 1] += 1 - pcloud(img[2], 6)
 
     # done
     if verbose:
