d96b1df5bc938fc695dc80bd1b8e7d0e9dd0abe0
xjtang <xjtang@bu.edu>
2017-07-07 10:52:21 -0400

    Added new csv2dict function

--- io/__init__.py
+++ io/__init__.py
@@ -2,7 +2,8 @@
 """
 from .viirs import viirs2gtif, viirsQA, vn2ln, viirsGeo
 from .stack import percent_cloudy
-from .datafile import csv2list
+from .datafile import csv2list, csv2dict
+from .shape import csv2shape
 
 
 __all__ = [
@@ -11,5 +12,7 @@ __all__ = [
     'vn2ln',
     'viirsGeo',
     'percent_cloudy',
-    'csv2list'
+    'csv2list',
+    'csv2dict',
+    'csv2shape'
 ]
