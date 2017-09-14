91728a0cd9153ea932c65b301aead7d281d1e85c
xjtang <xjtang@bu.edu>
2017-09-14 11:26:09 -0400

    Added functions and modules to convert HLS to stack image

--- common/constants.py
+++ common/constants.py
@@ -15,6 +15,11 @@ SR_BANDS = (23, 24, 25)
 QA_BANDS = (13, 14, 16, 18, 19)
 VZA_BAND = 1
 
+# HLS
+L30_BANDS = ('band02', 'band03', 'band04', 'band05', 'band06', 'band07',
+                'band09', 'QA')
+S30_BANDS = ('B02', 'B03', 'B04', 'B8A', 'B11', 'B12', 'B10', 'QA')
+
 # download
 _HTTP = 'https://e4ftl01.cr.usgs.gov/'
 _FTP = 'ftp://ladsweb.nascom.nasa.gov/'
--- io/__init__.py
+++ io/__init__.py
@@ -4,6 +4,7 @@ from .viirs import viirs2gtif, viirsQA, vn2ln, viirsGeo
 from .stack import stack2array, stack2image, stackGeo, array2stack
 from .datafile import csv2list, csv2dict
 from .shape import csv2shape
+from .hls import hls2stack, hlsQA, hn2ln
 
 
 __all__ = [
@@ -17,5 +18,8 @@ __all__ = [
     'csv2shape',
     'stack2image',
     'stackGeo',
-    'array2stack'
+    'array2stack',
+    'hn2ln',
+    'hls2stack',
+    'hlsQA'
 ]
