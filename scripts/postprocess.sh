f9924ec1cc952e55c0e0d12b5321e1a59b5b3368
xjtang <xjtang@bu.edu>
2017-09-30 16:01:16 -0400

    Added bash script for post process

---
+++ scripts/postprocess.sh
@@ -0,0 +1,42 @@
+#!/bin/bash
+
+# bash script to postprocess vnrt results
+
+# Input Arguments:
+#		-w window size, how much to extent out from center pixel
+#		-t clean up threhold
+#		--overwrite overwrite
+#		ori: origin
+#		des: destination
+
+# default values
+w=1
+t=2
+overwrite=''
+
+# parse input arguments
+while [[ $# > 0 ]]; do
+	InArg="$1"
+	case $InArg in
+		-2)
+			w=$2
+			shift
+			;;
+		-t)
+			t=$2
+			shift
+			;;
+		--overwrite)
+			overwrite='--overwrite '
+			;;
+		*)
+      ori=$1
+			des=$2
+			break
+	esac
+	shift
+done
+
+# submit jobs
+echo 'Submitting job for postprocessing.'
+qsub -N Postprocess -V -b y cd /projectnb/landsat/users/xjtang/documents/';' python -m VNRT.postprocess ${overwrite}-w $w -t $t $ori $des
