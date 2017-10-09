55778d5139d3f7310ca1b637b9d6e44293dec17f
xjtang <xjtang@bu.edu>
2017-10-09 14:31:38 -0400

    Added constants for his mask related things

--- common/constants.py
+++ common/constants.py
@@ -19,6 +19,11 @@ VZA_BAND = 1
 L30_BANDS = ('band02', 'band03', 'band04', 'band05', 'band06', 'band07',
                 'band09', 'QA')
 S30_BANDS = ('B02', 'B03', 'B04', 'B8A', 'B11', 'B12', 'B10', 'QA')
+MASK_NODATA = 255
+MASK_CLOUD = 4
+MASK_SHADOW = 2
+MASK_WATER = 1
+MASK_SNOW = 3
 
 # download
 _HTTP = 'https://e4ftl01.cr.usgs.gov/'
