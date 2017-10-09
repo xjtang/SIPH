032c3a1e334b5b53d1ffb3e01f1457592d09d0fe
xjtang <xjtang@bu.edu>
2017-10-09 14:31:14 -0400

    Moved io module of HLS to his folder

--- io/__init__.py
+++ io/__init__.py
@@ -2,9 +2,8 @@
 """
 from .viirs import viirs2gtif, viirsQA, vn2ln, viirsGeo
 from .stack import stack2array, stack2image, stackGeo, array2stack
-from .datafile import csv2list, csv2dict
+from .datafile import csv2list, csv2dict, hdr2geo
 from .shape import csv2shape
-from .hls import hls2stack, hlsQA, hn2ln
 from .cache import cache2map
 
 
@@ -20,8 +19,6 @@ __all__ = [
     'stack2image',
     'stackGeo',
     'array2stack',
-    'hn2ln',
-    'hls2stack',
-    'hlsQA',
-    'cache2map'
+    'cache2map',
+    'hdr2geo'
 ]
