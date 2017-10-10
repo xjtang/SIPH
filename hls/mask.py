9cf91003755ca159ebc19ba9db41cb3e8f9f3a4d
xjtang <xjtang@bu.edu>
2017-10-09 22:35:30 -0400

    Bug fix

--- hls/mask.py
+++ hls/mask.py
@@ -56,20 +56,20 @@ def bit2mask(bit, _source):
     """
     mask = np.zeros(bit.shape[-2:], np.int16)
     if _source == 'lasrc':
-        mask[bit == 255] = cons.MASK_NODATA
         mask[np.mod(np.right_shift(bit, 5), 2) > 0] = cons.MASK_WATER
         mask[np.mod(np.right_shift(bit, 4), 2) > 0] = cons.MASK_SNOW
         mask[np.mod(np.right_shift(bit, 3), 2) > 0] = cons.MASK_SHADOW
         mask[np.mod(bit, 8) > 0] = cons.MASK_CLOUD
+        mask[bit == 255] = cons.MASK_NODATA
     elif _source == 'fmask':
         return bit
     elif _source == 'maja':
-        mask[bit[0] == 255] = cons.MASK_NODATA
         mask[np.mod(np.right_shift(bit[0], 2), 4) > 0] = cons.MASK_SHADOW
         mask[np.mod(np.right_shift(bit[0], 4), 16) > 0] = cons.MASK_CLOUD
         mask[np.mod(np.right_shift(bit[0], 1), 2) > 0] = cons.MASK_CLOUD
         mask[np.mod(bit[1], 2) > 0] = cons.MASK_WATER
         mask[np.mod(np.right_shift(bit[1], 5), 2) > 0] = cons.MASK_SNOW
+        mask[bit[0] == 255] = cons.MASK_NODATA
     else:
         log.error('Unknown source: {}'.format(_source))
     return mask
