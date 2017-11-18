ba899905f9da68d4f04ddc846d58da4cb45e0ac9
xjtang <xjtang@bu.edu>
2017-11-17 21:29:42 -0500

    Bug fix

--- scripts/preprocess2.sh
+++ scripts/preprocess2.sh
@@ -52,5 +52,5 @@ done
 echo 'Total jobs to submit is' $njob
 for i in $(seq 1 $njob); do
     echo 'Submitting job no.' $i 'out of' $njob
-    qsub -j y -N Preprocess_$i -V -b y cd /projectnb/landsat/users/xjtang/documents/';' python -m SIPH.models.vnrt.preprocess2 ${overwrite}${recursive}-p $pattern -Q mgq -b $i $njob $ori $des
+    qsub -j y -N Preprocess_$i -V -b y cd /projectnb/landsat/users/xjtang/documents/';' python -m SIPH.models.vnrt.preprocess2 ${overwrite}${recursive}-p $pattern -Q $mgq -b $i $njob $ori $des
 done
