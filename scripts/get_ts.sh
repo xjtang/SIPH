c55bf73fae9f64ab262cd0811a469c7a71cb8bda
xjtang <xjtang@bu.edu>
2018-03-28 12:11:26 -0400

    Added new script and module to get time series from YATSM cache

---
+++ scripts/get_ts.sh
@@ -0,0 +1,35 @@
+#!/bin/bash
+
+# bash script to get time series
+
+# Input Arguments:
+#		--overwrite overwrite
+#   x: x
+#   y: y
+#		ori: origin
+#		des: destination
+
+# default values
+x=1
+y=1
+overwrite=''
+
+# parse input arguments
+while [[ $# > 0 ]]; do
+	InArg="$1"
+	case $InArg in
+		--overwrite)
+			overwrite='--overwrite '
+			;;
+		*)
+      x=$1
+      y=$2
+      ori=$3
+			des=$4
+			break
+	esac
+	shift
+done
+
+# submit jobs
+qsub -j y -N GetTS -V -b y cd /projectnb/landsat/users/xjtang/documents/';' python -m SIPH.tools.get_ts ${overwrite} $x $y $ori $des
