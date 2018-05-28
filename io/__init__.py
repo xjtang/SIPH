15c006ab8b8ddb2ba19d3dfdcae4118094b1ded1
xjtang <xjtang@bu.edu>
2018-05-28 15:04:22 -0400

    Added new function to add text to png image

--- io/__init__.py
+++ io/__init__.py
@@ -4,8 +4,7 @@ from .datafile import csv2list, csv2dict, hdr2geo, nc2array
 from .sif import sifn2ln, sif2stack, sif2grid, sifn2date
 from .goes import gn2ln, goes2stack
 from .viirs import viirs2gtif, viirsQA, vn2ln, viirsGeo
-from .stack import (stack2array, stack2image, stackGeo, array2stack, stackMerge,
-                    stack2table)
+from .stack import stack2array, stackGeo, array2stack, stackMerge, stack2table
 from .shape import csv2shape
 from .cache import cache2map, yatsm2map
 from .hls import hls2stack, hlsQA, hn2ln, ln2tn
@@ -13,6 +12,7 @@ from .mask import mn2ln, bit2mask, mask2array, mask2strata
 from .sentinel import sen2stack, sn2ln
 from .modis import (modis2stack, modis2composite, modisvi2stack, modislc2stack,
                     nbar2stack, pheno2stack, nbarcmg2stack)
+from .image import stack2image, addTextToImage
 
 
 __all__ = [
@@ -55,5 +55,6 @@ __all__ = [
     'nbarcmg2stack',
     'gn2ln',
     'goes2stack',
-    'yatsm2map'
+    'yatsm2map',
+    'addTextToImage'
 ]
