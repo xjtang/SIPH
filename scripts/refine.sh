0e63256441836431079239cd4e7eb57ec2baeb8c
xjtang <xjtang@bu.edu>
2018-09-30 21:50:36 -0400

    add script to refine classification results

---
+++ scripts/refine.sh
@@ -0,0 +1,33 @@
+#!/bin/bash
+
+# bash script to refine classification results
+
+# Input Arguments:
+#		--overwrite overwrite
+#		ori: origin
+#   lc: MODIS land cover stack
+#		des: destination
+
+# default values
+overwrite=''
+recursive=''
+
+# parse input arguments
+while [[ $# > 0 ]]; do
+	InArg="$1"
+	case $InArg in
+		--overwrite)
+			overwrite='--overwrite '
+			;;
+		*)
+      ori=$1
+      lc=$2
+			des=$3
+			break
+	esac
+	shift
+done
+
+# submit job
+echo 'Submitting job to refine classification results'
+qsub -j y -N Refine -V -b y cd /projectnb/landsat/users/xjtang/documents/';' python -m SIPH.models.chart.refine ${overwrite}-p $ori $lc $des
