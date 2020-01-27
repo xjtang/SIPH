cbc42eb3168c6b08f07e02ab8cb578c470cd5f50
xjtang <xjtang@bu.edu>
2020-01-27 15:28:01 -0500

    update refining routine

--- models/chart/refine.py
+++ models/chart/refine.py
@@ -63,26 +63,20 @@ def refine_results(ori, lc, vcf, des, overwrite=False):
 
     # refine classification results
     log.info('Refining maps...')
-    try:
+    if True:
+    #try:
         (lines, samples, nband) = r.shape
         for i in range(0,lines):
             for j in range(0, samples):
                 p = r[i, j, :]
+                p_class = np.unique(p)[0]
 
-                plc = np.zeros(nband, np.int8)
-                plc[0:18] = lc[i, j, :]
-                plc[18] = lc[i, j, -1]
-                for k in range(0, len(plc)):
-                    plc[k] = m2c[plc[k]]
+                plc = lc[i, j, 0:16]
                 plc_label = np.bincount(plc).argmax()
                 plcn = len(np.unique(plc))
 
-                pvcf = np.zeros(nband, np.int8)
-                pvcf[0:18] = vcf[i, j, 1:]
-                pvcf[18] = vcf[i, j, -1]
-
+                pvcf = vcf[i, j, 1:17]
                 mvcf = pvcf.mean()
-                p_class = np.unique(p)[0]
 
                 # fix short plantation in beginning
                 if p[3] != 18:
@@ -97,12 +91,12 @@ def refine_results(ori, lc, vcf, des, overwrite=False):
                 # deel with unclassified
                 if max(p == 0) == 1:
                     uclc = plc[p == 0]
-                    p_label = np.bincount(uclc).argmax()
+                    p_label = m2c[np.bincount(uclc).argmax()]
                     # mostly uc
-                    if sum(p == 0) > 8:
+                    if sum(p == 0) > 10:
                         p[p == 0] = p_label
                     # uc in the beginning
-                    elif (sum(p == 0) <= 3) & (p[0] == 0):
+                    elif (p[0] == 0):
                         if p_label in [13, 25]:
                             p[p == 0] = p_label
                         else:
@@ -117,26 +111,26 @@ def refine_results(ori, lc, vcf, des, overwrite=False):
                     elif sum(p == 0) < 3:
                         for k in range(0, len(p)):
                             if p[k] == 0:
-                                p[k] == p[k-1]
+                                p[k] == p[k - 1]
                     r[i, j, :] = p
 
                 # urban barren fix
                 if sum((p == 13) & (plc == 16)) >= 5:
-                    p[(p==13)&(plc==16)] = 16
+                    p[(p == 13) & (plc == 16)] = 16
                     r[i, j, :] = p
                 # urban grassland fix
                 if sum((p == 13) & (plc == 10)) >= 5:
-                    p[(p==13)&(plc==10)] = 10
+                    p[(p == 13) & (plc == 10)] = 10
                     r[i, j, :] = p
 
                 # urban check 2
                 if sum((p == 13) & (plc != 13)) >= 8:
                     if plc_label == 10:
-                        p[(p==13)&(plc!=13)] = 10
+                        p[(p == 13) & (plc != 13)] = 10
                     elif plc_label == 10:
-                        p[(p==13)&(plc!=13)] = 12
+                        p[(p == 13) & (plc != 13)] = 12
                     elif plc_label == 12:
-                        p[(p==13)&(plc!=13)] = 12
+                        p[(p == 13) & (plc != 13)] = 12
                     r[i, j, :] = p
 
                 # stable class check
@@ -146,7 +140,7 @@ def refine_results(ori, lc, vcf, des, overwrite=False):
                         if mvcf >= 60:
                             if plcn == 1:
                                 if plc_label <= 5:
-                                    p_label = plc_label
+                                    p_label = m2c[plc_label]
                                 else:
                                     p_label = 4
                             else:
@@ -155,7 +149,7 @@ def refine_results(ori, lc, vcf, des, overwrite=False):
                         elif mvcf >= 40:
                             if plcn == 1:
                                 if plc_label <= 5:
-                                    p_label = plc_label
+                                    p_label = m2c[plc_label]
                                 else:
                                     p_label = 4
                         elif mvcf <= 15:
@@ -189,12 +183,96 @@ def refine_results(ori, lc, vcf, des, overwrite=False):
                     if p_label > 0:
                         r[i, j, :] = p_label
 
+                # refinement 2
+                p = r[i, j, :]
+                psta = toSta(p[2], p[13])
+                p_label = -1
+                if psta == 3:
+                    if (plc_label in [2, 4, 5]):
+                        p_label = m2c[plc_label]
+                elif psta == 6:
+                    if (plc_label == 10) & (plcn == 1):
+                        p_label = 10
+                elif psta == 7:
+                    if (plc_label == 10) & (mvcf > 10) & (plcn == 1):
+                        p_label = 10
+                elif psta == 9:
+                    if (plc_label in [2, 4]) & (mvcf >= 40):
+                        p_label = m2c[plc_label]
+                    if (plc_label == 12) & (plcn == 1):
+                        p_label = 12
+                    if (plc_label in [8, 9]):
+                        if plcn == 1:
+                            p_label = 9
+                        if (plcn == 2) & (mvcf <= 20):
+                            p_label = 9
+                elif psta == 10:
+                    if (plc_label == 10) & (plcn == 1):
+                        p_label = 12
+                    if (plc_label == 12) & (plcn <= 2):
+                        p_label = 12
+                    if (plc_label == 9) & (plcn == 1):
+                        p_label = 9
+                    if (plc_label == 2) & (mvcf > 40) & (p[13] == 12):
+                        p[p == 12] = 18
+                elif psta == 11:
+                    if (plc_label == 2) & (p[2] in [2, 9]) & (p[13] == 18):
+                        if plcn == 1:
+                            p_label = 2
+                        if (plcn == 2) & (mvcf >= 45):
+                            p_label = 2
+                    if (plc_label in [10, 12]) & (p[13] == 18):
+                        if (plcn <= 2) & (mvcf <= 10) & (p[2] in [12, 9]):
+                            p_label = 12
+                        if (plcn >= 3):
+                            p[p == 18] = 12
+                    if (plc_label == 8):
+                        if (plcn == 1):
+                            p_label = 9
+                        if (plcn == 2) & (mvcf < 40):
+                            p_label = 9
+                elif psta == 12:
+                    if (plc_label == 13) & (plcn == 1):
+                        if (mvcf <= 5) & (p[2] == 12):
+                            p_label = 13
+                        if (mvcf > 10) & (p[2] == 17):
+                            p_label = 13
+                elif psta == 13:
+                    if (plc_label == 11) & (plcn <= 2):
+                        p_label = 19
+                    if (plc_label == 2) & (plcn <= 2) & (mvcf > 40) & (p[2] == 2):
+                        p_label = 2
+                elif psta == 14:
+                    if (plc_label in [10, 12]) & (p[2] in [10, 12]):
+                        if mvcf < 15:
+                            p_label = 12
+                        else:
+                            p_label = 9
+                    if (plc_label in [8, 9]) & (p[2] in [10, 12]):
+                        p_label = 9
+                elif psta == 15:
+                    if (plcn == 1) & (p[2] in [12, 17]) & (p[13] in [12, 17]):
+                        p_label = p[2]
+                    if (plcn <= 2) & (p[2] == 18) & (p[13] in [2, 9]):
+                        if (plc_label in [2, 4]) & (mvcf >= 40):
+                            p_label = plc_label
+                        if (plc_label == 8) & (mvcf >= 45):
+                            p_label = 2
+                        if (plc_label in [8, 9]) & (mvcf < 45):
+                            p_label = 9
+                        if (plc_label in [12, 10]) & (mvcf > 20):
+                            p_label = 9
+                    if (mvcf > 40) & (plcn <= 2) & (p[2] == 9) & (p[13] == 2):
+                        p_label = 9
+                if p_label > 0:
+                    r[i, j, :] = p_label
+
             progress = show_progress(i, lines, 5)
             if progress >= 0:
                 log.info('{}% done.'.format(progress))
-    except:
-        log.error('Failed to refine results.')
-        return 3
+    #except:
+    #    log.error('Failed to refine results.')
+    #    return 3
 
     # write output
     log.info('Writing output...')
