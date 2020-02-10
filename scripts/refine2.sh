e84e01e0a55dd65d0c2cbfcea8e30fab04cd17c6
xjtang <xjtang@bu.edu>
2020-02-10 16:17:11 -0500

    refine for krishna

---
+++ scripts/refine2.sh
@@ -0,0 +1,35 @@
+#!/bin/bash
+
+# bash script to refine classification results
+
+# Input Arguments:
+#		--overwrite overwrite
+#		ori: origin
+#   lc: MODIS land cover stack
+#		vcf: MODIS vcf stack
+#		des: destination
+
+# default values
+overwrite=''
+recursive=''
+
+# parse input arguments
+while [[ $# > 0 ]]; do
+	InArg="$1"
+	case $InArg in
+		--overwrite)
+			overwrite='--overwrite '
+			;;
+		*)
+      ori=$1
+      lc=$2
+			vcf=$3
+			des=$4
+			break
+	esac
+	shift
+done
+
+# submit job
+echo 'Submitting job to refine classification results'
+qsub -l mem_total=94G -j y -N Refine -V -b y cd /projectnb/landsat/users/xjtang/documents/';' python -m SIPH.models.chart.refine2 ${overwrite} $ori $lc $vcf $des
