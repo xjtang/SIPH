1e0f99962deb5bde0b061fa8dbc11c1b16250078
xjtang <xjtang@bu.edu>
2020-06-08 22:30:46 -0400

    bug fix

--- models/chart/refine.py
+++ models/chart/refine.py
@@ -63,8 +63,7 @@ def refine_results(ori, lc, vcf, des, overwrite=False):
 
     # refine classification results
     log.info('Refining maps...')
-    if True:
-    #try:
+    try:
         (lines, samples, nband) = r.shape
         for i in range(0,lines):
             for j in range(0, samples):
@@ -216,9 +215,9 @@ def refine_results(ori, lc, vcf, des, overwrite=False):
             progress = show_progress(i, lines, 5)
             if progress >= 0:
                 log.info('{}% done.'.format(progress))
-    #except:
-    #    log.error('Failed to refine results.')
-    #    return 3
+    except:
+        log.error('Failed to refine results.')
+        return 3
 
     # write output
     log.info('Writing output...')
